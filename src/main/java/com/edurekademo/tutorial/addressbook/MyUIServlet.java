bca76d341d97280f979ede8acb1e307c6632f8aa
BharatDasa <dasabharat90@gmail.com>
2026-04-08 05:43:36 -0700

    Create MyUIServlet.java

---
+++ src/main/java/com/edurekademo/tutorial/addressbook/MyUIServlet.java
@@ -0,0 +1,10 @@
+package com.edurekademo.tutorial.addressbook;
+
+import javax.servlet.annotation.WebServlet;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.annotations.VaadinServletConfiguration;
+
+@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = AddressbookUI.class, productionMode = false)
+public class MyUIServlet extends VaadinServlet {
+}
