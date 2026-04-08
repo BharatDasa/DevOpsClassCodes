d8507725191c63b1a9e77fc2935faa012c638b1b
BharatDasa <dasabharat90@gmail.com>
2026-04-08 06:04:58 -0700

    Update MyUIServlet.java

--- src/main/java/com/edurekademo/tutorial/addressbook/MyUIServlet.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/MyUIServlet.java
@@ -1,10 +1,6 @@
 package com.edurekademo.tutorial.addressbook;
 
-/*import javax.servlet.annotation.WebServlet;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.annotations.VaadinServletConfiguration;
-*/
-import jakarta.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.annotations.VaadinServletConfiguration;
 
